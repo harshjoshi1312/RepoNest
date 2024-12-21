188072a26e2eb0c3f5ea9c12ef45afc3bd3daf74
harshjoshi1312 <joshiharsh1312@gmail.com>
2024-12-21 16:17:24 +0530

    :sparkles: add repo initialize path

--- backend/controllers/init.js
+++ backend/controllers/init.js
@@ -1,6 +1,23 @@
+const fs = require("fs").promises;
+const path = require("path");
+const { json, buffer } = require("stream/consumers");
+
 async function initRepo() {
-    console.log("init function called");
-    
+  const repoPath = path.resolve(process.cwd(), ".demodir");
+  const commitsPath = path.join(repoPath, "commits");
+
+  try {
+    await fs.mkdir(repoPath, { recursive: true });
+    await fs.mkdir(commitsPath, { recursive: true });
+    await fs.writeFile(
+      path.join(repoPath, "config.json"),
+      JSON.stringify({ bucket: process.env.S3_BUCKET })
+    );
+    console.log("Repository has been initialized");
+  } catch (err) {
+    console.error("Error initializing repository");
+  }
 }
 
-module.exports = {initRepo};
+module.exports = { initRepo };
+// easy just making path for the init
