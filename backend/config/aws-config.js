317e7f98dc1404fd02c2445096a64b5d8a1e2b3f
harshjoshi1312 <joshiharsh1312@gmail.com>
2024-12-21 19:00:50 +0530

    aws swt up

---
+++ backend/config/aws-config.js
@@ -0,0 +1,8 @@
+const AWS = require("aws-sdk");
+
+AWS.config.update({ region: "ap- south - 1" });
+
+const S3 = new AWS.s3();
+const S3_BUCKET = "bucketname";
+
+module.exports = { s3, S3_BUCKET };
