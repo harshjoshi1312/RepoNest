030cc7b91e42467a896579cf1bf7dd3ec94e44e0
harshjoshi1312 <joshiharsh1312@gmail.com>
2024-12-21 16:34:17 +0530

    :white_check_mark: add stage logiv

--- backend/controllers/add.js
+++ backend/controllers/add.js
@@ -1,5 +1,18 @@
-async function addRepo() {
-  console.log(" file added succesfully");
+const fs = require("fs").promises;
+const path = require("path");
+
+async function addRepo(filePath) {
+  const repoPath = path.resolve(process.cwd(), ".demodir");
+  const stagePath = path.join(repoPath, "staging");
+
+  try {
+    await fs.mkdir(stagePath, { recursive: true });
+    const fileName = path.basename(filePath);
+    await fs.copyFile(filePath, path.join(stagePath, fileName));
+    console.log(`FIle ${fileName} added to the staging area`);
+  } catch (error) {
+    console.error("Error adding file : ", error);
+  }
 }
 
 module.exports = { addRepo };
